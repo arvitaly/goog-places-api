c73d76b72d6ecef9734aab1db8212053799a624a
Alexandro <arvitaly@list.ru>
2017-09-24 01:36:12 +0300

    add exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,8 @@
 import details from "./details";
 import nearby from "./nearby";
 import photo from "./photo";
+export { ILocation as IPlaceDetails, IPhoto, IReview } from "./details";
+export { ILocation as IPlaceNearby } from "./nearby";
 export default (key: string) => ({
     details: details(key),
     photo: photo(key),
